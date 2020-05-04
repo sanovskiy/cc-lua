d048eb653129a53ec0a99003a70877d1b7d60f7b
Pavel Terentyev <pavel.terentyev@gmail.com>
2020-05-04 23:43:09 +0700

    Updates

--- src/san-get.lua
+++ src/san-get.lua
@@ -150,7 +150,6 @@ local actions = {
   selfupdate = function (opts)
       updateRepo()
       installSoftware("san-get")
-      print("XYU!")
     end,
   install =  function (opts)
       local softName = opts[1]
