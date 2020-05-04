9e092dcf0d3d9ceb162c27380ce943a7d6ba4f45
Pavel Terentyev <pavel.terentyev@gmail.com>
2020-05-05 05:17:21 +0700

    WIP: Updated

--- src/api/ic2/reactors.lua
+++ src/api/ic2/reactors.lua
@@ -4,18 +4,22 @@ local entityIDs = {"ic2:nuclear reactor", "ic2:reactor chamber"}
 
 local allEntities = {}
 
-for _,_v in pairs(entityIDs) do
-	for __,_p in pairs(_v) do
-		table.insert(allEntities,_p)
-	end
+for _, _v in pairs(entityIDs) do
+  for __, _p in pairs(peripheral.find(_v)) do
+    table.insert(allEntities, _p)
+  end
 end
 
 function getAllEntities(){
-	return allEntities
+  return allEntities
 }
 
 function getUnitsCount()
   return #allEntities
+end
+
+function hasEmptySlots(entity)
+
 end
 
 --function getTotalEnergy()
