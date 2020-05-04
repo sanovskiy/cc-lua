ee79a2358354bdfbc1a6a2b191213e132a45d624
Pavel Terentyev <pavel.terentyev@gmail.com>
2020-05-05 05:08:24 +0700

    WIP: Updated

--- src/ic2/reactor-monitor.lua
+++ src/ic2/reactor-monitor.lua
@@ -1,5 +1,8 @@
-os.loadAPI("/lib/mfsu")
+os.loadAPI("/lib/ic2batbox")
+os.loadAPI("/lib/ic2reactors")
 
-print("Units: "..mfsu.getUnitsCount())
-print("Stored energy: "..mfsu.getTotalEnergy())
-print("Total capacity: "..mfsu.getTotalCapacity())
+print("Energy units: "..ic2batbox.getUnitsCount())
+print("Stored energy: "..ic2batbox.getTotalEnergy())
+print("Total capacity: "..ic2batbox.getTotalCapacity())
+
+print("Reactor units"..ic2reactors.getUnitsCount())
