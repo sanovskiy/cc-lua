cf9cb2d36cc5dce378fe6a8082c0dc487ca92311
Pavel Terentyev <pavel.terentyev@gmail.com>
2020-05-05 00:59:02 +0700

    Updates

--- src/api/mfsu.lua
+++ src/api/mfsu.lua
@@ -1,4 +1,25 @@
 os.loadAPI('/lib/debug')
 
-local periNames = peripheral.getNames()
-debug.vardump(periNames)
+local entityID = "ic2:mfsu"
+
+allEntities = { peripheral.find(entityID) }
+
+function getUnitsCount()
+	return #allEntities
+end
+
+function getTotalEnergy()
+	local result = 0
+	for _,v in pairs(allEntities) do
+		result = result + v.getEUStored()
+	end
+	return result
+end
+
+function getTotalCapacity()
+	local result = 0
+	for _,v in pairs(allEntities) do
+		result = result + v.getCapacity()
+	end
+	return result
+end
---
+++ src/mfsumon.lua
@@ -0,0 +1,5 @@
+os.loadAPI("/lib/mfsu")
+
+print("Units: "..mfsu.getUnitsCount())
+print("Stored energy: "..mfsu.getTotalEnergy())
+print("Total capacity: "..mfsu.getTotalCapacity())
