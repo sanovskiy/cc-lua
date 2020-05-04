297232ee2d85e40c5b0efaa5f3ba73263ccb8907
Pavel Terentyev <pavel.terentyev@gmail.com>
2020-05-04 21:35:04 +0700

    Added repo

--- repo.lua
+++ repo.lua
@@ -1,22 +1,20 @@
-local repo = {
-	{
-		name = "san-get",
-		files = {
-			{
-				url = "src/san-get.lua",
-				localname = "/san-get.lua"
-			}
-		}
-	},
-	{
-		name = "san-gh",
-		files = {
-			{
-				url = "src/san-gh.lua",
-				localname = "san-gh.lua"
-			}
-		}
-	}
+{
+  "san-get": {
+    "files": [
+      {
+        "url":"src/san-get.lua",
+        "localname":"/san-get.lua",
+      }
+    ]
+    "requres":["san-gh"]
+  },    
+  "san-gh": {
+    "files": [
+      {
+        "url":"src/san-gh.lua",
+        "localname":"san-gh.lua",
+      }
+    ]
+    "requres":[]
+  }
 }
-
-return repo
